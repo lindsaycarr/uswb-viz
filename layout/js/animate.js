555fcdca5cdc5284217d5e392db709a2bf583207
David Blodgett <dblodgett@usgs.gov>
2017-10-15 11:25:10 -0500

    put in clicklink here for now

--- layout/js/animate.js
+++ layout/js/animate.js
@@ -6,3 +6,12 @@ function clearEmphasis(){
   $(".emphasis").removeClass("emphasis");
   hovertext(" ");
 }
+
+function clicklink(url, event){
+ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+   event.stopPropagation();
+  }else{
+    ga('send', 'event', 'outbound', 'click', url);
+    window.open(url, '_blank');
+  }
+}
